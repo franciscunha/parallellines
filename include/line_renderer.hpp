bb0904152fef38b5d7ca3ab7cfbf1a471c8d89d0
Francisco Cunha <francisccdn@gmail.com>
2024-12-02 18:34:02 +0100

    cleanup line renderer namespace

--- include/line_renderer.hpp
+++ include/line_renderer.hpp
@@ -3,7 +3,7 @@
 #include "tgaimage.hpp"
 #include "model.hpp"
 
-namespace line_render
+namespace line_renderer
 {
     void draw(Vec2i p0, Vec2i p1, TGAImage &output, TGAColor color);
     void wireframe(Model &model, TGAImage &output, TGAColor color);
