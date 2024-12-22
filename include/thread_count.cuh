58e96eb4422a6982dc805bc9a235ad58baa9d7f1
Francisco Cunha <francisccdn@gmail.com>
2024-12-22 20:33:49 +0100

    make calculate_kernel_size inline

--- include/thread_count.cuh
+++ include/thread_count.cuh
@@ -4,7 +4,7 @@
 
 #define TARGET_GRID_SIZE 16 // value depends on GPU, check NVIDIA Nsight compute
 
-void calculate_kernel_size(size_t target_num_threads, size_t *out_num_blocks, size_t *out_num_threads_per_block)
+inline void calculate_kernel_size(size_t target_num_threads, size_t *out_num_blocks, size_t *out_num_threads_per_block)
 {
     *out_num_threads_per_block = std::ceil(target_num_threads / (float)TARGET_GRID_SIZE);
     size_t remainder = (*out_num_threads_per_block) % 32;
